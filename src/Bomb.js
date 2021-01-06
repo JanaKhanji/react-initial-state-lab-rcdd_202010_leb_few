33a1a2d50495e95eadd61ccf0a9d56a95c918e29
Jana Khanji <janokhanji@gmail.com>
2021-01-06 12:55:54 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,11 +1,9 @@
 // your Bomb code here!
 import React from 'react';
 
-<<<<<<< HEAD
+
 export default class Bomb extends React.Component {
-=======
-class Bomb extends React.Component {
->>>>>>> 9ccf604927b507a60aa40280aed8e6dd1e8c5a35
+
 
   constructor(props) {
     super() 
